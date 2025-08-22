3e4c8610b5dcdfb6f22facd4189a44cd42f7377c
Vladimir Koltsov <grunch2008@gmail.com>
2025-08-22 16:43:42 +0300

    move stopwatch to the adder.go

--- adder.go
+++ adder.go
@@ -1,5 +1,34 @@
 package myMath
 
+import (
+	"time"
+)
+
 func Add(a, b int) int {
 	return a + b
+}
+
+type Stopwatch struct {
+	started time.Time
+
+	Results []time.Duration
+	Stopped bool
+}
+
+func (s *Stopwatch) Start() {
+	if s.Stopped {
+		s.Results = make([]time.Duration, 0)
+		s.started = time.Now()
+	}
+	s.Stopped = true
+}
+
+func (s *Stopwatch) SaveSplit() {
+	if !s.Stopped {
+		s.Results = append(s.Results, time.Since(s.started))
+	}
+}
+
+func (s Stopwatch) GetResults() []time.Duration {
+	return s.Results
 }
